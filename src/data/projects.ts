7aaadaa989ae68fc953daa13526c23ffe6d4d633
ViniciusLvr <dev.vncs@gmail.com>
2026-04-16 11:38:54 -0400

    refact/Add projeto e melhores cores

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -20,10 +20,19 @@ export const projects: Project[] = [
     comingSoon: false,
   },
   {
+    id: "svce",
+    title: "Sistema de Vendas e Controle de Estoque",
+    description:
+      "Este foi meu projeto de conclusão do curso Técnico em Informática, um sistema de controle de estoque e simulação de vendas feito em PHP e pondo em prática vários conceitos aprendidos em sala de aula, como: Banco de Dados Relacional e Servidores Locais em Linux (ubuntu server).",
+    technologies: ["PHP", "MySQL", "Linux"],
+    codeUrl: "https://github.com/ViniciusLvr/SVCE",
+    comingSoon: false,
+  },
+  {
     id: "blog",
     title: "Blog de games",
     description:
-      "Um blog simples onde quero postar opniões e comentar sobre jogos que ando jogando, filmes, animes e talvez algumas series. Coisa bem intima e pessoal que quero criar na marra ao inves de usar alguma outra plataforma focada no mesmo objetivo.",
+      "Um blog simples onde quero postar opiniões e comentar sobre jogos que ando jogando, filmes, animes e talvez algumas series. Coisa bem intima e pessoal que quero criar na marra ao inves de usar alguma outra plataforma focada no mesmo objetivo.",
     technologies: ["React"],
     comingSoon: true,
   },
